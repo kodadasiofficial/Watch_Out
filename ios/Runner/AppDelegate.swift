a595d1ca01b0a9b6fdcee6ab16926952b11f75d7
kodadasiofficial <isaadahanunal@gmail.com>
2024-02-20 19:49:49 +0300

    google maps ios integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBxpJIJ1WtONrCMKWHZPOt58i4gBPGQ7sI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
